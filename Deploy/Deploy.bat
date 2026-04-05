c284446e9a80ed39990ef5ba474766845abd6299
BabyImpala <116924540+BabyImpala@users.noreply.github.com>
2026-04-05 17:17:56 +0500

    initial cleanup

--- Deploy/Deploy.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-SET ThisScriptsDirectory=%~dp0
-SET PSSctiptName=Deploy.ps1
-SET PowerShellScriptPath=%ThisScriptsDirectory%%PSSctiptName%
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%'";
